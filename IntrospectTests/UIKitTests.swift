d616add40a9fb0f7a17389719ee97d6a95632e9b
Philip Niedertscheider <dev@philpri.me>
2022-04-17 09:43:27 +0200

    Reverted removed formatting

--- IntrospectTests/UIKitTests.swift
+++ IntrospectTests/UIKitTests.swift
@@ -402,7 +402,7 @@ class UIKitTests: XCTestCase {
     }
     
     func testTabViewRoot() {
-            
+        
         let expectation = XCTestExpectation()
         let view = TabRootTestView(spy: {
             expectation.fulfill()
